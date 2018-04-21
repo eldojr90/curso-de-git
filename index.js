3e974e9f62ec80b5cb62b5353b0712c3dca8df37
Eldo Jr <eldojr90@gmail.com>
2018-04-21 20:36:07 -0300

    Atualização conteúdo

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
 console.log("qq coisa");
+console.log("qq coisa 2 o retorno");
+console.log("qq coisa 3 o retorno da volta do retorno");
+console.log("qq coisa 4 o retorno da volta do retorno isso ai");
